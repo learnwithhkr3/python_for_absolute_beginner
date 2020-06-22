db7236cd0052203c12f65ab8a4e2b857eeef5925
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-06-22 17:28:06 +0530

    tut12.py

---
+++ tut12.py
@@ -0,0 +1,22 @@
+# print("Welcome to My channel Learnwithhkr")
+
+# if else statement
+
+# age = int(input("Enter your age "))
+
+# if age > 50:
+#     print("you are not eligible for voting")
+# elif age > 18:
+#     print("your are eligible for voting")
+# else:
+#     print("you are not eligible for voting")
+
+# print( age == 18 )
+
+
+l = [25,36,96,52]
+
+if 25 not in l:
+    print("number found in list")
+else:
+    print("number not found")

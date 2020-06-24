f4a4d1c9c0c9882454179be6880df00405cc8d38
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-06-24 18:46:16 +0530

    tut13.py

---
+++ tut13.py
@@ -0,0 +1,26 @@
+# Given a name, return a string with the message:
+name = "talib"
+
+# if name is given
+# "hello talib"
+
+# if the name is missing
+# "hello everyone"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(Greet(name))

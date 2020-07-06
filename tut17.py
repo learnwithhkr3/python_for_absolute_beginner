7b557f7243bebb42e08672112cb1e75d0e84ec0c
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-06 18:57:47 +0530

    Operators

---
+++ tut17.py
@@ -0,0 +1,60 @@
+# Arthmetic operators
+#  +, -, /, *, %, **, //
+
+# a = 2
+# b = 2
+
+# print("a + b = ",a+b)
+# print("a - b = ",a-b)
+# print("a / b = ",a/b)
+# print("a * b = ",a*b)
+# print("a % b = ",a%b)
+# print("a ** b = ",a**b)
+# print("a // b = ",a//b)
+
+# Assignment Operators
+#  =, +=, -=, *=, %=, **=, //=
+# a = 10
+# a += 5 # a = a % 5
+
+# print(a)
+
+# Comparison Operators
+#  ==, !=, >, >=, <, <=
+
+a = 10
+b = 10
+c = 12
+
+# Logical Operators
+#  and, or, not
+
+# print(not(a>b))
+
+# Identity Operators
+#  is, is not
+
+# list1 = [10,20,30]
+# list2 = [10,20,30]
+
+# x = list1
+
+# print(list2 is not list1)
+ 
+
+# Membarship Operators 
+#  in, not in
+
+# fruits = ["banana","mango","apple"]
+
+# print("xyz" not in fruits)
+
+# Bitwise Operators
+#  &, |, <<, >>
+# 0,1
+
+print(bin(10))
+print(bin(12))
+print(bin(14))
+print(10 | 12)
+

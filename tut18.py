9f693b30c459338153d3bc2d5dd341f50b7f1b20
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-09 19:20:29 +0530

    add tut18.py

---
+++ tut18.py
@@ -0,0 +1,20 @@
+# break 
+# continue
+
+# ui = int(input("How many candy You want:- "))
+
+# i = 1
+# while(i <= ui):
+#     if i > 10:
+#         print("Sorry Out of Stock")
+#         break
+#     print("Candy",i)
+    # i += 1 #i = i +1
+
+l = ["banana","apple","orange","mango","orange"]
+
+for item in l:
+    if item == "orange":
+        continue
+    print(item)
+

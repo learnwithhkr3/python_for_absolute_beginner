9b0375c32de671f1fadd966b5d48f0a801636203
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-17 11:14:20 +0530

    Add tut21.py

---
+++ tut21.py
@@ -0,0 +1,12 @@
+num1 = int(input("emter first number:-"))
+num2 = int(input("emter second number:-"))
+
+
+try:
+    print("Resilt:- ", num1/num2)
+except Exception as e:
+    print(e)
+
+
+
+print("Thank you for running this code")

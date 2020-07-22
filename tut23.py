ecdb7a3048e0ededf91c25466f772793ed5cc3fa
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-22 15:14:53 +0530

    Add Tut23.py

---
+++ tut23.py
@@ -0,0 +1,21 @@
+# f = open('image.jpg', 'rb')
+
+# nf = open('new.jpg','wb')
+
+# # print(f.read())
+
+# nf.write(f.read())
+
+
+# nf.close()
+# f.close()
+
+
+f = open('newfile.txt','r+')
+
+f.write("please subscribe channel")
+
+# print(f.read())
+
+
+f.close()

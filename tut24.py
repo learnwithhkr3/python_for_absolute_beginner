8fff031e433edba0e632bacb0586cce3a4c94690
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-24 07:53:48 -0700

    add tut24.py

---
+++ tut24.py
@@ -0,0 +1,14 @@
+with open('newfile.txt','rt') as f:
+    f.seek(34)
+    print(f.readline(),end='')
+    # print(f.readline(),end='')
+    # print(f.tell())
+
+
+
+# f.write("please subscribe channel")
+
+# print(f.read())
+
+
+# f.close()

8b6275b7a7acaa3a0cc7849afe51012406138f0d
hkr hasan <learnwithhkr@gmail.com>
2020-08-03 16:18:53 +0530

    add tut31.py

---
+++ tut31.py
@@ -0,0 +1,14 @@
+firstName = "Hkr"
+lastName = "Hasan"
+
+# My name is
+age = 22
+
+# sentence = "My name is " + firstName + " " + lastName + "age is " + str(age)
+
+# sentence = "My name is {0} {1} Age is {2}".format(firstName, lastName, age)
+
+sentence = f"My name is {firstName} {lastName} Age is {265.144:.1f}"
+
+print(sentence)
+
